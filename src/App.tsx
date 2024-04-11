f44ab7062437db25e701328dd474015d263aa2da
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-04-11 13:58:32 +0100

    Update App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import React from "react";
 
 function App() {
-	return <h1 className="text-3xl font-bold underline">Client Manager</h1>;
+	return (
+		<>
+			<h1 className="text-[3rem]">Client Manager</h1>
+		</>
+	);
 }
 
 export default App;

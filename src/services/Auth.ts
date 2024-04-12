eefb69a88ef3e3802d2877959854a798aec8de6b
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-04-12 15:52:19 +0100

    Add authentication services

---
+++ src/services/Auth.ts
@@ -0,0 +1,52 @@
+import axiosInstance from "@/interceptors";
+
+export interface CredentialsProps {
+	firstName?: string;
+	lastName?: string;
+	email: string;
+	password: string;
+	securePasswordFlag?: boolean;
+}
+
+async function loginUser(credentials: CredentialsProps) {
+	let result = {};
+
+	await axiosInstance
+		.post("/auth/login", credentials)
+		.then((response) => {
+			result = response.data;
+		})
+		.catch((err) => {
+			result = err.response.data;
+		});
+
+	return result;
+}
+
+async function logoutUser() {
+	const result = await axiosInstance
+		.get("/auth/logout")
+		.catch((err) => console.log(err));
+
+	return result;
+}
+
+async function registerUser(data: CredentialsProps) {
+	let result = {};
+
+	await axiosInstance
+		.post("/auth/create-user", data)
+		.then((response) => {
+			result = response.data;
+		})
+		.catch((err) => {
+			result = err.response.data;
+			return;
+		});
+
+	return result;
+}
+
+async function checkToken() {}
+
+export { loginUser, logoutUser, registerUser, checkToken };

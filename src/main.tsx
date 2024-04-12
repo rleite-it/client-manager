eb9653ea5e5eef3f02658583f56bf5cc39ad4967
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-04-12 17:58:04 +0100

    Add user and client provider to main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "@/components/theme-provider";
-import "@/styles/globals.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./hooks/useAuth.tsx";
+import { ThemeProvider } from "@/components/theme-provider";
+import { AuthProvider } from "@/hooks/useAuth.tsx";
+import { UserProvider } from "@/hooks/useUser.tsx";
+import { ClientProvider } from "@/hooks/useClient.tsx";
 import App from "./App.tsx";
+
+import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 			<BrowserRouter>
 				<AuthProvider>
-					<App />
+					<UserProvider>
+						<ClientProvider>
+							<App />
+						</ClientProvider>
+					</UserProvider>
 				</AuthProvider>
 			</BrowserRouter>
 		</ThemeProvider>

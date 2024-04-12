66cdc521e13cb939e56ff33bcfd9b13caf5adab1
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-04-12 15:50:33 +0100

    Add custom protected route

---
+++ src/routes/protected-route.tsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./../hooks/useAuth";
+
+// We are taking in the component that should be rendered if the user is authed
+// We are also passing the rest of the props to the <Route /> component such as
+// exact & the path
+const ProtectedRoute = ({ children }) => {
+	// Getting the value from our cool custom hook
+	const { authed } = useAuth();
+
+	return authed ? <>{children}</> : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;

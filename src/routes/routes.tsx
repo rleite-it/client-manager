2880721e394140c42faf107d4377fc9170077311
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-04-12 21:23:02 +0100

    Add client details route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,12 +2,14 @@ import { lazy } from "react";
 import ProtectedRoute from "./protected-route";
 
 const Dashboard = lazy(() => import("@/pages/dashboard"));
+const ClientDetails = lazy(() => import("@/pages/client-details"));
 const Profile = lazy(() => import("@/pages/profile"));
 const Login = lazy(() => import("@/pages/login"));
 const Error = lazy(() => import("@/pages/error"));
 
 export const ROUTES = [
 	{
+		name: "dashboard",
 		path: "/",
 		element: (
 			<ProtectedRoute>
@@ -17,6 +19,17 @@ export const ROUTES = [
 		errorElement: <Error />,
 	},
 	{
+		name: "client-details",
+		path: "/client/:id",
+		element: (
+			<ProtectedRoute>
+				<ClientDetails />
+			</ProtectedRoute>
+		),
+		errorElement: <Error />,
+	},
+	{
+		name: "profile",
 		path: "/profile",
 		element: (
 			<ProtectedRoute>
@@ -26,6 +39,7 @@ export const ROUTES = [
 		errorElement: <Error />,
 	},
 	{
+		name: "login",
 		path: "/login",
 		element: <Login />,
 		errorElement: <Error />,
